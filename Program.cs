8c5a95b1bb7172b278ba1554345cf3553ad80047
Nikon <109473540+Hololitt@users.noreply.github.com>
2023-01-15 23:58:37 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,53 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace ConsoleApp4
 {
-    internal class Program
+   internal class Program
     {
-        static void Main(string[] args)
+          static void Main(string[] argc)
         {
-            Console.WriteLine("Enter word and value that will change with word");
-            Console.WriteLine("Word: ");
-            string word = Console.ReadLine();
-            Console.WriteLine("Value: ");
-            int value = Convert.ToInt32(Console.ReadLine());
-            Function function = new Function(word,value);
-            Console.WriteLine(function.start());
+            int a = 22;
+            String s ="sda";
+            Console.WriteLine($"werwer {a} {s}");
             Console.ReadKey();
         }
-        class Function
-        {
-            private string word;
-            private int value;
-            public Function(string word, int value)
-            {
-                this.word = word;
-                this.value = value;
-          
-            }
-          public String start()
-            {
-                try
-                {
-                    value = Convert.ToInt32(word);
-                    word = Convert.ToString(value);
-                }
-                catch(System.FormatException e)
-                {
-                    Console.WriteLine("Error");
-                }
-              
-
-                return ($"Value: {this.value} Word: {this.word}");
-            }
-
-            }
-            
-        }
     }
+}
