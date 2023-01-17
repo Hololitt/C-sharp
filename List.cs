23d18b77ecab29d4ed7203d5250284ba5edbc674
Nikon <109473540+Hololitt@users.noreply.github.com>
2023-01-17 22:12:04 +0100

    Add files via upload

---
+++ List.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test3
+{
+    internal class CaseAlternatorTask
+    {
+        static void Main(string[] argc)
+        {
+           
+        }
+        static void Solver(char[] word, int startIndex, List<string> result)
+        {
+            var subWord = word.ToArray();
+            if (startIndex == word.Length)
+            {
+                result.Add(new string(word));
+                return;
+            }
+
+            var lower = char.ToLower(word[startIndex]);
+            var upper = char.ToUpper(word[startIndex]);
+            word[startIndex] = lower;
+            Solver(word, startIndex + 1, result);
+            if (lower != upper)
+            {
+                word[startIndex] = upper;
+                Solver(word, startIndex + 1, result);
+            }
+        }
+    }
+
+}
