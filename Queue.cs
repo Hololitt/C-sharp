2c535e7376e4d25025f4b4894229f1f04812e197
Nikon <109473540+Hololitt@users.noreply.github.com>
2023-01-19 23:17:26 +0100

    Add files via upload

---
+++ Queue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("c");
+            queue.Enqueue("a");
+            queue.Enqueue("b");
+
+           queue.Dequeue();
+
+           foreach(string itrem in queue)
+            {
+                Console.WriteLine(itrem);
+                
+            }
+        }
+    }
+}
