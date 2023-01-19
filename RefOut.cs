b77bff44758539b8b244a08073da0b874338d703
Nikon <109473540+Hololitt@users.noreply.github.com>
2023-01-19 19:31:56 +0100

    Add files via upload

---
+++ RefOut.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test6
+{
+    internal class Program
+    {
+      static void func(int a, int b, ref int result)
+        {
+            result = (int)Convert.ToDouble(Math.Pow(a, b));
+        }
+        static void func2(int value1, int value2, out int result2)
+        {
+            result2 = value1 / value2;
+        }
+        static void Main(string[] args)
+        {
+            int a = 2, b = 12 ,result = 0;
+            func(a, b, ref result);
+            Console.WriteLine(result);
+
+            /////////////
+
+            int value1 = 100, value2 = 2, result2;
+            func2(value1, value2, out result2);
+            Console.WriteLine(result2);
+            Console.ReadKey();
+
+        }
+
+    }
+}
