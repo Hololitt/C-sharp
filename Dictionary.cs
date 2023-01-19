8828b46f9d265b71b038fc44a3492b2ec26a4077
Nikon <109473540+Hololitt@users.noreply.github.com>
2023-01-19 23:15:37 +0100

    Add files via upload

---
+++ Dictionary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("word", "translation");
+            dictionary.Add("word2", "translation2");
+            if(dictionary.ContainsKey("word")){
+                Console.WriteLine(dictionary["word"]);
+            }
+
+            foreach(var words in dictionary) {
+                Console.WriteLine($"Key: {words.Key}  Value: {words.Value}");
+                }
+            Console.ReadKey();
+        }
+        }
+    }
+
