6bd090e173e957226b01ddad212d663827d3129f
Nikon <109473540+Hololitt@users.noreply.github.com>
2023-01-19 23:18:47 +0100

    Add files via upload

---
+++ Stack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Stack
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            foreach(int i in stack)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(stack.Peek());
+            stack.Pop();
+            Console.WriteLine(stack.Peek());
+
+            Console.ReadKey();
+        }
+    }
+}
