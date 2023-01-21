d984d258a277e0e2931f8d386602a4ccd7c64960
Nikon <109473540+Hololitt@users.noreply.github.com>
2023-01-21 18:51:45 +0100

    Add files via upload

---
+++ Properties.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human();
+           int humanId= human.setId = 1;
+            Console.WriteLine(humanId);
+            Console.ReadKey();
+        }
+    }
+    class Human
+    {
+        private int id;
+        public int setId
+        {
+            set
+            {
+                id = value;
+            }
+            get
+            {
+                return id;
+            }
+        }
+    }
+}
