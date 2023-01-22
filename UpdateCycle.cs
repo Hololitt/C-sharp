7ee8aab55f1974d97eac9e6881ac74e617338bda
Nikon <109473540+Hololitt@users.noreply.github.com>
2023-01-22 01:32:22 +0100

    Add files via upload

---
+++ UpdateCycle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UpdateCycles
+{
+    internal class UpdateCycle
+    {
+        static void Main(string[] args)
+        {
+            Machine[] machines = { new FirstMachine(), new SecondMachine()};
+           while(true)
+            {
+               foreach(var machine in machines) { machine.work();
+                    Thread.Sleep(1000);
+                }
+            }
+            
+        }
+        class Machine
+        {
+            public virtual void work()
+            {
+                Console.WriteLine("It`s machine");
+            }
+        }
+        class FirstMachine : Machine
+        {
+            public override void work()
+            {
+                base.work();
+                Console.WriteLine("It`s first machine!");
+            }
+        }
+        class SecondMachine : Machine
+        {
+            public override void work()
+            {
+                base.work();
+                Console.WriteLine("It`s second machine!");
+            }
+        }
+    }
+}
